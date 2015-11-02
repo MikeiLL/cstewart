8d0bcb890f13c8b0538be43cd54e0d43597580e0
Mike iLL Kilmer <mike@mzoo.org>
2015-11-02 12:44:09 -0600

    Add gulp flag.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -avP dist lib screenshot.png templates *.php *.css poman:public_html/wp/wp-content/themes/poman
+rsync -avP dist lib screenshot.png templates *.php *.css cstewart:public_html/wp/wp-content/themes/stewart
