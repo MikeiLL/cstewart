569490e049b32015697324ce210b0ea5cccbf081
Mike iLL Kilmer <mike@mzoo.org>
2016-01-31 20:27:13 -0600

    Let's make it a container-fluid.

--- base.php
+++ base.php
@@ -19,7 +19,7 @@ use Roots\Sage\Extras;
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-    <div class="wrap container" role="document">
+    <div class="wrap container-fluid" role="document">
       <div class="content row">
         <main class="main" role="main">
           <?php include Wrapper\template_path(); ?>
